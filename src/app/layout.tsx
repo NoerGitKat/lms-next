dae390b6e93fa8dfc3380441dc5614a27ba4b8f8
Noer Paanakker <noerpaanakker@gmail.com>
2024-09-22 10:07:39 +0200

    Added next starter + rtk

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { ReduxProvider } from "@/redux/provider";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "LMS by Noer",
+  description: "A dynamic LMS to create different online courses with.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ReduxProvider>{children}</ReduxProvider>
+      </body>
+    </html>
+  );
+}
